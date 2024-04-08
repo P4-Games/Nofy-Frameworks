5836af4c7c7f39a1a6156d991990165a4ed4215e
Martin H. Pefaur <mpefaur@gmail.com>
2024-04-08 10:57:08 -0300

    Update Rank Api

--- app/api/rank/route.ts
+++ app/api/rank/route.ts
@@ -11,31 +11,8 @@ const getUsersRank = async () => {
     // Obtener todos los usuarios
     const userRanking = await usersCollection.find().toArray()
 
-    // Ordenar los usuarios por rango de medallas y número de personajes
-    const sortedRanking = userRanking.sort((a: any, b: any) => {
-        // Calcular el rango de medallas para cada usuario
-        const aMedalRank = calculateMedalRank(a.medals)
-        const bMedalRank = calculateMedalRank(b.medals)
-
-        // Comparar el rango de medallas de los usuarios
-        if (aMedalRank > bMedalRank) {
-            return -1
-        } else if (aMedalRank < bMedalRank) {
-            return 1
-        } else {
-            // Si los usuarios tienen el mismo rango de medallas, comparar por cantidad de personajes
-            if (a.characters.length > b.characters.length) {
-                return -1
-            } else if (a.characters.length < b.characters.length) {
-                return 1
-            } else {
-                return 0 // Los usuarios son iguales en rango de medallas y cantidad de personajes
-            }
-        }
-    })
-
     // Limitar el ranking a los primeros 10 jugadores
-    const limitedRanking = sortedRanking.slice(0, 10)
+    const limitedRanking = userRanking.slice(0, 10)
 
     // Crear el objeto de ranking con la posición como clave
     const rankingByPosition: Record<string, any> = {};
@@ -43,27 +20,11 @@ const getUsersRank = async () => {
         rankingByPosition[(index + 1).toString()] = {
             Nick: user.nick,
             Characters: user.characters.length,
-            Medals: user.medals
         };
     });
 
     return rankingByPosition;
 }
-
-// Función para calcular el rango de medallas de un usuario
-const calculateMedalRank = (medals: any) => {
-    let medalRank = 0
-
-    // Sumar los rangos de todas las medallas del usuario
-    medalRank += countMedals(medals, 'gold') * 3 // Cada medalla de oro vale 3 puntos
-    medalRank += countMedals(medals, 'silver') * 2 // Cada medalla de plata vale 2 puntos
-    medalRank += countMedals(medals, 'bronze') * 1 // Cada medalla de bronce vale 1 punto
-
-    return medalRank
-}
-
-// Función para contar la cantidad de un tipo de medalla específico
-const countMedals = (medals: any, medalType: any) => medals.filter((medal: any) => medal === medalType).length
 
 // Controlador de la API
 export async function GET(req: NextRequest, res: NextResponse) {
